48d448d77ac2a86faf74e645fae5769d7aa05a65
Tea-Script <mciama1996@gmail.com>
2016-12-03 14:55:54 -0500

    Update NoColor.js

--- NoColor.js
+++ NoColor.js
@@ -1,30 +1,42 @@
-var canvas = document.createElement('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-canvas.style.position = "fixed";
-canvas.style.zIndex = 10;
-canvas.style.pointerEvents = "none";
+//NoColor.js
 
-document.body.insertBefore(canvas, document.body.firstChild);
-
-var c = document.querySelector("canvas")
-var ctx = c.getContext('2d');
 
 var width = window.innerWidth;
 var height = window.innerHeight;
-var rainbow = true;
+var body = document.querySelector("body");
 
-var interval = setInterval(drawColor, 100);
+var canvas = document.createElement('canvas');
 
-function drawColor(){
+canvas.width = width;
+canvas.height = height;
+canvas.style.zIndex   = Number.MAX_SAFE_INTEGER;
+canvas.style.position = "fixed";
+canvas.style.pointerEvents = "none";
+document.body.insertBefore(canvas, document.body.firstChild);
 
-	if (rainbow){
-		var r = Math.round(255*Math.random());
-		var g = Math.round(255*Math.random());
-		var b = Math.round(255*Math.random());
-		var rgb = [r, g, b];
-		ctx.fillStyle = 'rgb(' + rgb.join(",") + ')';
+
+var c = document.querySelector("canvas");
+var ctx = c.getContext("2d");
+
+ctx.fillStyle = "black";
+var i =200;
+var j = 200;
+var rainbowBool = true;
+var repeat =setInterval(fillPixels,100);
+function fillPixels(){
+	/*fills in random sets of pixels based on screen size*/
+	if(rainbowBool){ 
+		var r = Math.round(255*(Math.random()));
+		var g = Math.round(255*(Math.random()));
+		var b = Math.round(255*(Math.random()));
+		var rgb = [r,g,b];
+		ctx.fillStyle = "rgb(" + rgb.join(",") +")";
 	}
 
-	ctx.fillRect((Math.random() * width), (Math.random() * height), Math.round(width/500), Math.round(height/500));
+	ctx.fillRect(i,j,Math.round(width/500),Math.round(height/500));
+	i = Math.round(Math.random()*width);             // generate a random number between 0 and width
+	j = Math.round(Math.random()*height);             // generate a random number between 0 and height
+
+
 }
+
