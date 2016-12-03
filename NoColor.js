9ceb1243e93f6bd3759af4f8c1c6b45fa2215e67
Tea-Script <mciama1996@gmail.com>
2016-12-03 14:54:24 -0500

    Update NoColor.js

--- NoColor.js
+++ NoColor.js
@@ -1,28 +1,47 @@
-var canvas = document.createElement('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-canvas.style.position = "absolute";
-canvas.style.zIndex = Number.MAX_SAFE_INTEGER;
-document.body.appendChild(canvas);
-
-var c = document.querySelector("canvas")
-var ctx = c.getContext('2d');
-
+//NoColor.js
 var width = window.innerWidth;
 var height = window.innerHeight;
-var rainbow = true;
+var body = document.querySelector("body");
+//body.setAttribute("style", "height: "+ height+"px");
+//body.setAttribute("style", "width: "+ width+"px");
 
-var interval = setInterval(drawColor, .000000000001);
 
-function drawColor(){
+var canvas = document.createElement('canvas');
+//canvas.id = "mycanvas";
+canvas.width = width//document.body.clientWidth;
+canvas.height = height//document.body.clientHeight;
+canvas.style.border = "1px solid";
+canvas.style.zIndex   = Number.MAX_SAFE_INTEGER;
+canvas.style.position = "fixed";
+canvas.style.pointerEvents = "none";
+document.body.insertBefore(canvas, document.body.firstChild);
 
-	if (rainbow){
-		var r = Math.round(255*Math.random());
-		var g = Math.round(255*Math.random());
-		var b = Math.round(255*Math.random());
-		var rgb = [r, g, b];
-		ctx.fillStyle = 'rgb(' + rgb.join(",") + ')';
+
+var c = document.querySelector("canvas");
+var ctx = c.getContext("2d");
+
+ctx.fillStyle = "black";
+var i =200;
+var j = 200;
+var rainbowBool = true;
+var repeat =setInterval(fillPixels,100);
+function fillPixels(){
+	/*fills in random sets of pixels based on screen size*/
+	if(rainbowBool){ 
+		var r = Math.round(255*(Math.random()));
+		var g = Math.round(255*(Math.random()));
+		var b = Math.round(255*(Math.random()));
+		var rgb = [r,g,b];
+		ctx.fillStyle = "rgb(" + rgb.join(",") +")";
 	}
 
-	ctx.fillRect((Math.random() * width), (Math.random() * height), Math.round(width/500), Math.round(height/500));
+	ctx.fillRect(i,j,Math.round(width/500),Math.round(height/500));
+	i = Math.round(Math.random()*width);             // generate a random number between 0 and width
+	j = Math.round(Math.random()*height);             // generate a random number between 0 and height
+
+	//canvas.width = window.innerWidth
+	//canvas.height = window.innerHeight
+
+
 }
+
