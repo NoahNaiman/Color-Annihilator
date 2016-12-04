001ebb22de9c4afdf28f559d9c34c81d23023e0c
NoahNaiman <nnaiman@bu.edu>
2016-12-03 20:58:46 -0500

    Errors when attempting to stop annhilitating.

--- NoColor.js
+++ NoColor.js
@@ -21,9 +21,9 @@ var ctx = c.getContext("2d");
 ctx.fillStyle = "black";
 var i =200;
 var j = 200;
-var rainbowBool = true;
+var rainbowBool = false;
 var blackBool = false;
-var whiteBool = false;
+var whiteBool = true;
 var blackAndWhiteBool = false;
 var repeat =setInterval(fillPixels,100);
 function fillPixels(){
@@ -45,11 +45,29 @@ function fillPixels(){
 		var rgb = [255,255,255];
 		ctx.fillStyle = "rgb(" + rgb.join(",") +")";
 	}
-	else if(blackAndWhiteBool){
-		
-	}
 
 	ctx.fillRect(i,j,Math.round(width/500),Math.round(height/500));
 	i = Math.round(Math.random()*width);             // generate a random number between 0 and width
 	j = Math.round(Math.random()*height);             // generate a random number between 0 and height
 }
+
+var data = '<svg xmlns="http://www.w3.org/2000/svg" width="200" height="200">' +
+           '<foreignObject width="100%" height="100%">' +
+           '<div xmlns="http://www.w3.org/1999/xhtml" style="font-size:40px">' +
+             markup +
+           '</div>' +
+           '</foreignObject>' +
+           '</svg>';
+
+var DOMURL = window.URL || window.webkitURL || window;
+
+var img = new Image();
+var svg = new Blob([data], {type: 'image/svg+xml'});
+var url = DOMURL.createObjectURL(svg);
+
+img.onload = function () {
+  ctx.drawImage(img, 0, 0);
+  DOMURL.revokeObjectURL(url);
+}
+
+img.src = url;
